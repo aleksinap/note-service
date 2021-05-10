b7e70e749cc47cdeccf13071e3382b0a0aeacba8
aleksinap <aleksinap@yandex.ru>
2021-05-10 22:52:01 +0300

    Initial Commit

--- src/main/kotlin/ru/netology/noteservice/NoteService.kt
+++ src/main/kotlin/ru/netology/noteservice/NoteService.kt
@@ -10,10 +10,13 @@ class UserNotFoundException(message: String) : RuntimeException(message)
 class AddingCommentException(message: String) : RuntimeException(message)
 class GettingCommentsException(message: String) : RuntimeException(message)
 class NoCommentsException(message: String) : RuntimeException(message)
+class GettingNotesException(message: String) : RuntimeException(message)
 
 class NoteService {
-    private var pairs: MutableList<GenericPair<Note,MutableList<Comment>>> =
-        emptyList<GenericPair<Note,MutableList<Comment>>>().toMutableList()
+    private var pairs = mutableListOf<GenericPair<Note,MutableList<Comment>>>()
+    private var guids = mutableListOf<String>()
+    private var deletedPairs = mutableListOf<GenericPair<Note,MutableList<Comment>>>()
+    private var deletedComments = mutableListOf<Comment>()
 
     //  Создает новую заметку у текущего пользователя.
     fun add(
@@ -23,9 +26,9 @@ class NoteService {
         privacyComment: List<String> = listOf("all"),
     ) : Int {
         if(pairs.isEmpty()){
-            pairs.add(GenericPair(Note(1,1, title, text, 123456,0,0,"https://netology.ru"), emptyList<Comment>().toMutableList()))
+            pairs.add(GenericPair(Note(1,1, title, text, 123456,0,0,"https://netology.ru"), mutableListOf()))
         } else {
-            pairs.add(GenericPair(Note(pairs.last().first.id + 1,1, title, text, 123456,0,0,"https://netology.ru"), emptyList<Comment>().toMutableList()))
+            pairs.add(GenericPair(Note(pairs.last().first.id + 1,1, title, text, 123456,0,0,"https://netology.ru"), mutableListOf()))
         }
         return pairs.last().first.id
     }
@@ -36,7 +39,7 @@ class NoteService {
         ownerId: Int,
         replyTo: Int?,
         message: String,
-        guid: String,
+        guid: String
     ) : Int{
         if(pairs.isNotEmpty()){
             for(pair in pairs){
@@ -44,76 +47,126 @@ class NoteService {
                     if(pair.second.isEmpty()){
                         pair.second.add(Comment(1,1,noteId,ownerId,123467,message, replyTo))
                     } else {
+                        for(g in guids){
+                            if(g == guid) throw AddingCommentException("Такой комметарий уже существует")
+                        }
                         pair.second.add(Comment(pair.second.last().cid + 1,1,noteId,ownerId,123467,message, replyTo))
                     }
                     pair.first = pair.first.copy(comments = pair.first.comments + 1)
+                    guids.add(guid)
                     return pair.second.last().cid
                 }
-
             }
-            for(pair in pairs){
-                if(pair.first.id != noteId){
-                    throw NoteNotFoundException("Заметка с таким id не существует")
-                } else {
-                    if(pair.first.ownerId != ownerId) {
-                        throw UserNotFoundException("Пользователя с таким id не существует")
-                    }
-                }
-            }
+            throwUserNotFoundException(ownerId)
+            throwNoteNotFoundException(noteId)
         }
         throw AddingCommentException("Попытка добавить комметарий к несуществующей заметке")
         return pairs.last().second.last().cid
     }
 
-    //    //  Удаляет заметку текущего пользователя.
-//    fun delete(noteId: Int) : Boolean {
-//        TODO("Создать функцию удаления заметки.")
-//        return true
-//    }
-//
-//    //    Удаляет комментарий к заметке.
-//    fun deleteComment(commentId: Int, ownerId: Int) : Boolean {
-//        TODO("Создать функцию удаления комментария к заметке.")
-//        return true
-//    }
-//
-//    //  Редактирует заметку текущего пользователя.
-//    fun edit(
-//        noteId: Int,
-//        title: String,
-//        text: String,
-//        privacyView: List<String> = listOf("all"),
-//        privacyComment: List<String> = listOf("all"),
-//    ) : Boolean {
-//        TODO("Создать функцию редактирования заметки.")
-//        return true
-//    }
-//
-//    //  Редактирует указанный комментарий у заметки.
-//    fun editComment(commentId: Int, ownerId: Int, message: String) : Boolean {
-//        TODO("Создать функцию редактирования указанного комментария к заметке.")
-//        return true
-//    }
-//
-//    //  Возвращает список заметок, созданных пользователем.
-//    fun get(noteIds: List<Int>, userId: Int, offset: Int = 0, count: Int = 20, sort: Int = 0): MutableList<GenericPair<Note, MutableList<Comment>?>> {
-//        TODO("Создать функцию возвращения списка заметок, созданных пользователем.")
-//        return pairs
-//    }
-//
-//    //  Возвращает заметку по её id.
-//    fun getById(noteId: Int, ownerId: Int, needWiki: Boolean = false): MutableList<GenericPair<Note, MutableList<Comment>?>> {
-//        TODO("Создать функцию возвращения заметки по её id.")
-//        return pairs
-//    }
-//
-//    //  Возвращает список комментариев к заметке.
-    fun getComments(noteId: Int, ownerId: Int, sort: Int = 0, offset: Int = 0, count: Int = 20): MutableList<Comment> {
-        var x: MutableList<Comment>
+    //  Удаляет заметку текущего пользователя.
+    fun delete(noteId: Int) : Boolean {
+        for(pair in pairs){
+            if(pair.first.id == noteId){
+                deletedPairs.add(pair)
+                pairs.remove(pair)
+                return true
+            }
+        }
+        return false
+    }
+
+    //    Удаляет комментарий к заметке.
+    fun deleteComment(commentId: Int, ownerId: Int) : Boolean {
+        for(i in pairs.indices){
+            for(j in pairs[i].second.indices) {
+                if(pairs[i].second[j].ownerId == ownerId && pairs[i].second[j].cid == commentId){
+                    deletedComments.add(pairs[i].second[j])
+                    pairs[i].second.remove(pairs[i].second[j])
+                    return true
+                }
+            }
+        }
+        throwUserNotFoundException(ownerId)
+        throwNoCommentsException(commentId)
+        return false
+    }
+
+    //  Редактирует заметку текущего пользователя.
+    fun edit(
+        noteId: Int,
+        title: String,
+        text: String,
+        privacyView: List<String> = listOf("all"),
+        privacyComment: List<String> = listOf("all"),
+    ) : Boolean {
         if(pairs.isNotEmpty()){
             for(pair in pairs){
-                if(pair.first.id == noteId && pair.first.ownerId == ownerId){
-                    if(pair.second.isEmpty()){
+                if(pair.first.id == noteId){
+                    pair.first = pair.first.copy(title = title, text = text)
+                    return true
+                }
+            }
+            throwNoteNotFoundException(noteId)
+        }
+        throwNoteNotFoundException(noteId)
+        return false
+    }
+
+    //  Редактирует указанный комментарий у заметки.
+    fun editComment(commentId: Int, ownerId: Int, message: String) : Boolean {
+        for(i in pairs.indices){
+            for(j in pairs[i].second.indices) {
+                if(pairs[i].second[j].ownerId == ownerId && pairs[i].second[j].cid == commentId){
+                    pairs[i].second[j] = pairs[i].second[j].copy(message = message)
+                    return true
+                }
+            }
+        }
+        throwUserNotFoundException(ownerId)
+        throwNoCommentsException(commentId)
+        return false
+    }
+
+    //  Возвращает список заметок, созданных пользователем.
+    fun get(noteIds: List<Int>, userId: Int, offset: Int = 0, count: Int = 20, sort: Int = 0): MutableList<Note> {
+        var list = mutableListOf<Note>()
+
+        if(pairs.isNotEmpty()){
+            for(i in noteIds.indices){
+                for(j in pairs.indices) {
+                    if(pairs[j].first.ownerId == userId && pairs[j].first.id == noteIds[i]) {
+                        list.add(pairs[j].first)
+                        break
+                    }
+                }
+            }
+            throwUserNotFoundException(userId)
+            noteIds.forEach { throwNoteNotFoundException(it) }
+            return list
+        }
+        throw GettingNotesException("Список заметок пуст!")
+        return list
+    }
+
+    //  Возвращает заметку по её id.
+    fun getById(noteId: Int, ownerId: Int, needWiki: Boolean = false): MutableList<Note> {
+        return get(listOf(noteId), ownerId)
+    }
+
+    //  Возвращает список комментариев к заметке.
+    fun getComments(
+        noteId: Int,
+        ownerId: Int,
+        sort: Int = 0,
+        offset: Int = 0,
+        count: Int = 20
+    ): MutableList<Comment> {
+        var x: MutableList<Comment>
+        if (pairs.isNotEmpty()) {
+            for (pair in pairs) {
+                if (pair.first.id == noteId && pair.first.ownerId == ownerId) {
+                    if (pair.second.isEmpty()) {
                         throw NoCommentsException("У заметки с таким id нет комметариев")
                     } else {
                         x = pair.second
@@ -121,24 +174,54 @@ class NoteService {
                     return x
                 }
             }
-
-            for(pair in pairs){
-                if(pair.first.id != noteId){
-                    throw NoteNotFoundException("Заметка с таким id не существует")
-                } else {
-                    if(pair.first.ownerId != ownerId) {
-                        throw UserNotFoundException("Пользователя с таким id не существует")
-                    }
-                }
-            }
+            throwUserNotFoundException(ownerId)
+            throwNoteNotFoundException(noteId)
         }
         throw GettingCommentsException("Попытка получить список комметариев к несуществующей заметке")
         return x
     }
-//
-//    //  Восстанавливает удалённый комментарий.
-//    fun restoreComment(commentId: Int, ownerId: Int): Boolean {
-//        TODO("Создать функцию восстановления удалённого комментария к заметке.")
-//        return true
-//    }
+
+    private fun throwNoteNotFoundException(noteId: Int) {
+        var isIdEqual = false
+        for (pair in pairs) {
+            if (pair.first.id == noteId) isIdEqual = true
+        }
+        if (!isIdEqual) throw NoteNotFoundException("Заметка с таким id не существует")
+    }
+
+    private fun throwUserNotFoundException(ownerId: Int) {
+        var isOwnerIdEqual = false
+        for (pair in pairs) {
+            if (pair.first.ownerId == ownerId) isOwnerIdEqual = true
+        }
+        if (!isOwnerIdEqual) throw UserNotFoundException("Пользователя с таким id не существует")
+    }
+
+    private fun throwNoCommentsException(commentId: Int) {
+        var isCommentIdEqual = false
+        for(i in pairs.indices){
+            for(j in pairs[i].second.indices) {
+                if(pairs[i].second[j].cid == commentId) isCommentIdEqual = true
+            }
+        }
+        if (!isCommentIdEqual) throw NoCommentsException("Комментария с таким id не существует")
+    }
+
+    //  Восстанавливает удалённый комментарий.
+    fun restoreComment(commentId: Int, ownerId: Int): Boolean {
+        for(i in deletedComments.indices) {
+            if(deletedComments[i].ownerId == ownerId && deletedComments[i].cid == commentId){
+                for(j in pairs.indices){
+                    if(pairs[i].first.ownerId == ownerId){
+                        pairs[i].second.add(deletedComments[i])
+                        deletedComments.remove(pairs[i].second[j])
+                        return true
+                    }
+                }
+            }
+        }
+        throwUserNotFoundException(ownerId)
+        throwNoCommentsException(commentId)
+        return false
+    }
 }
--- src/test/kotlin/ru/netology/noteservice/NoteServiceTest.kt
+++ src/test/kotlin/ru/netology/noteservice/NoteServiceTest.kt
@@ -1,10 +1,8 @@
 package ru.netology.noteservice
 
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
-import ru.netology.data.Comment
-import kotlin.reflect.KClass
 
 class NoteServiceTest {
 
@@ -39,7 +37,7 @@ class NoteServiceTest {
         val noteService = NoteService()
         noteService.add("Title", "Text")
         noteService.createComment(1,1,null,"message", "Text")
-        val lastComment = noteService.createComment(1,1,null,"message", "Text")
+        val lastComment = noteService.createComment(1,1,null,"message", "Texts")
         assertTrue(lastComment > 0)
     }
 
@@ -54,7 +52,8 @@ class NoteServiceTest {
     fun createCommentUserNotFoundException() {
         val noteService = NoteService()
         noteService.add("Title", "Text")
-        noteService.createComment(1,2,null,"message", "Text")
+        noteService.add("Title", "Text")
+        noteService.createComment(2,2,null,"message", "Text")
     }
 
     @Test(expected = AddingCommentException::class)
@@ -95,5 +94,140 @@ class NoteServiceTest {
     fun getCommentsShouldThrowGettingCommentsException() {
         val noteService = NoteService()
         noteService.getComments(1,5)
+    }
+
+    @Test
+    fun deleteCommentTrue() {
+        val noteService = NoteService()
+
+        noteService.add("Title", "Text")
+        noteService.createComment(1,1,null,"message", "guid")
+
+        assertTrue(noteService.deleteComment(1,1))
+    }
+
+    @Test
+    fun deleteTrue() {
+        val noteService = NoteService()
+
+        noteService.add("Title", "Text")
+
+        assertTrue(noteService.delete(1))
+    }
+
+    @Test
+    fun deleteFalse() {
+        val noteService = NoteService()
+        noteService.add("Title", "Text")
+        assertFalse(noteService.delete(2))
+    }
+
+    @Test(expected = NoCommentsException::class)
+    fun throwNoCommentsException() {
+        val noteService = NoteService()
+
+        noteService.add("Title", "Text")
+        noteService.createComment(1,1,null,"message", "guid")
+
+        noteService.deleteComment(6,1)
+    }
+
+    @Test
+    fun get() {
+        val noteService = NoteService()
+        noteService.add("Title", "Text")
+        noteService.add("Title", "Text")
+        noteService.get(listOf(1,2), 1)
+    }
+
+    @Test(expected = GettingNotesException::class)
+    fun getGettingNotesException() {
+        val noteService = NoteService()
+        noteService.get(listOf(1,2), 1)
+    }
+    @Test
+    fun getById() {
+        val noteService = NoteService()
+        noteService.add("Title", "Text")
+        noteService.getById(1, 1)
+    }
+
+    @Test
+    fun edit() {
+        val noteService = NoteService()
+        noteService.add("Title", "Text")
+        noteService.add("Title", "Text")
+        noteService.add("Title", "Text")
+
+        assertTrue(noteService.edit(1, "New title", "New text"))
+    }
+
+    @Test(expected = NoteNotFoundException::class)
+    fun editShouldThrowNoteNotFoundException() {
+        val noteService = NoteService()
+        noteService.edit(1, "New title", "New text")
+    }
+
+    @Test(expected = NoteNotFoundException::class)
+    fun editShouldThrowAnotherNoteNotFoundException() {
+        val noteService = NoteService()
+        noteService.add("Title", "Text")
+        noteService.edit(2, "New title", "New text")
+    }
+
+    @Test
+    fun editComment() {
+        val noteService = NoteService()
+        noteService.add("Title", "Text")
+        noteService.createComment(1,1,null,"message", "guid")
+        assertTrue(noteService.editComment(1,1,"New message"))
+    }
+
+    @Test(expected = NoCommentsException::class)
+    fun editCommentShouldThrowNoCommentsException() {
+        val noteService = NoteService()
+        noteService.add("Title", "Text")
+        noteService.createComment(1,1,null,"message", "guid")
+        noteService.editComment(2,1,"New message")
+    }
+
+    @Test(expected = UserNotFoundException::class)
+    fun editCommentShouldThrowUserNotFoundException() {
+        val noteService = NoteService()
+        noteService.add("Title", "Text")
+        noteService.createComment(1,1,null,"message", "guid")
+        noteService.editComment(1,2,"New message")
+    }
+
+    @Test
+    fun restoreComment() {
+        val noteService = NoteService()
+        noteService.add("Title", "Text")
+        noteService.add("Title", "Text")
+        noteService.add("Title", "Text")
+        noteService.createComment(1,1,null,"message", "guid")
+        noteService.deleteComment(1,1)
+        assertTrue(noteService.restoreComment(1,1))
+    }
+    @Test(expected = NoCommentsException::class)
+    fun restoreCommentShouldThrowNoCommentsException() {
+        val noteService = NoteService()
+        noteService.add("Title", "Text")
+        noteService.add("Title", "Text")
+        noteService.add("Title", "Text")
+        noteService.createComment(1,1,null,"message", "guid")
+        noteService.deleteComment(1,1)
+        noteService.restoreComment(2,1)
+    }
+
+    @Test(expected = UserNotFoundException::class)
+    fun restoreCommentShouldThrowUserNotFoundException() {
+        val noteService = NoteService()
+        noteService.add("Title", "Text")
+        noteService.add("Title", "Text")
+        noteService.add("Title", "Text")
+        noteService.createComment(1,1,null,"message", "guid")
+        noteService.deleteComment(1,1)
+        noteService.restoreComment(1,2)
     }
 }
